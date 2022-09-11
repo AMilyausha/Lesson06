612e99bbbcfbd7272b89804afda13ba071fe079f
Amirova Milyausha <mila17.86@mail.ru>
2022-09-11 22:12:11 +0300

    Первое задание

---
+++ Example001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Программа запрашивает натуральное число M, 
+// пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine("Сколько чисел будете вводить? ");
+int a = int.Parse(Console.ReadLine());
+int result=0;
+
+for (int i=0;i<a;i++)
+{
+    Console.WriteLine("Введите число: ");
+    int b= int.Parse(Console.ReadLine());
+    if (b>0) {
+        result=result+1;
+    }
+}
+Console.WriteLine("Положительных чисел : " +result);
